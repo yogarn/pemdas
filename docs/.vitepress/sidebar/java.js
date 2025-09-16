720f1fb708cc9077ba22227d8b97f9c7ceecf38f
Yoga Raditya <radityanalaa@gmail.com>
2025-09-16 20:48:51 +0700

    refactor: update sidebar code

--- docs/.vitepress/sidebar/java.js
+++ docs/.vitepress/sidebar/java.js
@@ -5,18 +5,37 @@ const javaSidebar = [
     text: `Let's Code!`,
     items: [
       {
+        text: `Get Started`,
+        link: `${path}/`
+      },
+      {
         text: `Struktur, Tipe Data, Konstanta, dan Variable Java`,
         link: `${path}/struktur_tipe_data`,
       },
-      { text: `Input & Output`, link: `${path}/input_output` },
+      {
+        text: `Input & Output`,
+        link: `${path}/input_output`
+      },
       {
         text: `Simple Process`,
         link: `${path}/simple_process`,
       },
-      { text: `Percabangan`, link: `${path}/seleksi_kondisi` },
-      { text: `Pengulangan`, link: `${path}/loop` },
-      { text: `Array`, link: `${path}/array` },
-      { text: `Method`, link: `${path}/method` },
+      {
+        text: `Percabangan`,
+        link: `${path}/seleksi_kondisi`
+      },
+      {
+        text: `Pengulangan`,
+        link: `${path}/loop`
+      },
+      {
+        text: `Array`,
+        link: `${path}/array`
+      },
+      {
+        text: `Method`,
+        link: `${path}/method`
+      },
     ],
   },
 ];
